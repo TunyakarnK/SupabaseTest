7f2ea716dcdf8446565ffc05647422cb85919fcc
TunyakarnK <tunyakarn2544@gmail.com>
2024-02-20 11:30:12 +0700

    shared with me pages

--- my-app/src/pages/SharedMeeting/SharedMeetingFoldder.jsx
+++ my-app/src/pages/SharedMeeting/SharedMeetingFoldder.jsx
@@ -2,12 +2,26 @@
 import { useEffect, useState } from "react";
 import { supabase } from '../../supabaseClient.js';
 import Navbar from '../../components/Navbar/Navbar.jsx'
+import React from 'react';
+import '../../components/MeetingCard.css'
+import { useNavigate, Link } from "react-router-dom";
+import MeetingCard from 'src/components/NewMeetingCard';
+import FolderCard from 'src/components/FolderCard';
+import { Grid, ScrollArea, TextInput,Text,rem, Button,Modal ,Radio,Group} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
 
 function SharedMeeting() {
-  // const navigate = useNavigate();
-
-    const [user, setUser] = useState({});
-
+  const navigate = useNavigate();
+  
+  const [user, setUser] = useState({});
+  const [createFolder, setCreateFolder] = useState(false);
+  const [folder, setFolder] = useState([]);
+  const [folderName, setFolderName] = useState([]);
+  const [newMeeting, setNewMeeting] = useState([]);
+  const [opened, { open, close }] = useDisclosure(false);
+  const [newMeetName, setNewMeetName] = useState([]);
+  
     useEffect(() =>{
       async function getUserData() {
         await supabase.auth.getUser().then((value) =>{
@@ -19,17 +33,91 @@ function SharedMeeting() {
         })
       }
       getUserData();
+      fetchFolder();
+      fetchNewMeeting();
+      console.log('meet'+newMeeting)
     }, [])
+
+    async function fetchNewMeeting() {
+      try {
+        const { data, error } = await supabase
+          .from("meeting")
+          .select("*")
+          //จริงๆๆต้องเอาแค่ meeting ที่พึ่งสร้างใหม่
+        if (error) throw error;
+        if (data != null) {
+          setNewMeeting(data); 
+        }
+      } catch (error) {
+        alert(error.message);
+      }
+    }
+
+    async function fetchFolder() {
+      try {
+        const { data, error } = await supabase
+          .from("folders")
+          .select("*")
+        if (error) throw error;
+        if (data != null) {
+          setFolder(data); 
+        }
+      } catch (error) {
+        alert(error.message);
+      }
+    }
+  
+    async function createNewFolder() {
+      try {
+        const { data, error } = await supabase
+          .from("folders")
+          .insert({
+            folderName: folderName
+          })
+          .single()
+          
+        if (error) throw error;
+        window.location.reload();
+      } catch (error) {
+        alert(error.message);
+      }
+    }
+  
+    console.log(folder);
       
     return (
       <div className="App">
         {Object.keys(user).length !== 0 ?
-          <>
+    <>
         <header>
         <Navbar props={user}/>
         </header>
-       <h1>This is SharedMeeting Page</h1>
-       </>
+
+       <div style={{backgroundColor:'#FDEFE9', margin:"50px", padding:'20px',minHeight:'500px'}}>   
+       <div>
+        <Grid align="center">
+          <Grid.Col span={10.4}><Text size='30px' fw={'500'} style={{marginTop:'50px',marginBottom:'30px'}}>Shared with me</Text></Grid.Col>
+        </Grid>
+ 
+        <div>
+          <Grid align="center" style={{ borderBottom: '1px solid black',paddingBottom:'10px'}}>
+          <Grid.Col span={5}><Text c="#4f5b5f" style={{marginLeft:'10px'}}>Folder Name</Text></Grid.Col>
+          <Grid.Col span={5.5} ><Text c="#4f5b5f">Owner</Text></Grid.Col>
+          </Grid>
+
+          <div>
+            {folder.map((folder) => (
+              <FolderCard folder = {folder} user = {user} key={folder.folderId} />
+            ))}
+            
+          </div>
+        </div>
+          
+            </div>
+            </div>
+            <div style={{height:'10px', backgroundColor:'white'}}></div> 
+           <div style={{height:'10px', backgroundColor:'#EE5D20',position: 'fixed',bottom: '0', width: '100%'}}></div> 
+    </>
        :
        <></>
        }
---
+++ my-app/src/pages/SharedMeeting/SharedMeetingList.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import MeetingCard from 'src/components/MeetingCard';
+import { useParams } from 'react-router-dom'
+import { useSession } from '@supabase/auth-helpers-react';
+import { useState, useEffect } from 'react';
+import Navbar from 'src/components/Navbar/Navbar';
+import { supabase } from 'src/supabaseClient';
+import { Grid, ScrollArea, TextInput,Text,rem, Button,Modal } from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
+
+function SharedMeetingList(props) { 
+  const { folderid } = useParams();
+  const navigate = useNavigate();
+  const [meeting, setMeeting] = useState([]);
+  const [folder, setFolder] = useState([]);
+  const [folderName, setFolderName] = useState([]);
+  const session = useSession();
+
+
+  // ตอน fetch folderName แล้วมัน Error
+  useEffect(() =>{
+    async function fetchFolder() {
+    try {
+      const { data, error } = await supabase
+          .from("folders")
+          .select()
+          .eq("folderId",folderid)
+          if (error) throw error;
+          if (data != null) {
+            setFolder(data);
+            console.log('data='+data)
+          }
+        } catch (error) {
+          alert(error.message);
+        }
+      }
+
+    fetchFolder();
+    fetchMeeting();
+    console.log(folder)
+    // console.log(folder[0].folderName)
+  }, [])
+
+  async function fetchMeeting() {
+    try {
+      const { data, error } = await supabase
+          .from("meeting")
+          .select("*")
+            //จริงๆๆต้องเอาแค่ meeting ที่พึ่งสร้างใหม่
+          if (error) throw error;
+          if (data != null) {
+          setMeeting(data); 
+          }
+        } catch (error) {
+          alert(error.message);
+        }
+      }
+
+      function statisticButton(){
+        navigate('/Folder/'+folder.folderid+'/statistic', { state: { folder } } );    
+      }
+  
+  
+  return (
+    <div>
+    <div className="App">
+        {Object.keys(session.user).length !== 0 ?
+          <>
+        <header>
+        <Navbar props={session.user} />
+        </header>
+
+        <div style={{backgroundColor:'#FDEFE9', margin:"40px", padding:'20px'}}>
+     
+        
+        <Grid align="center">
+        {/* <Grid.Col span={10.4}><Text size='30px' fw={'500'} style={{marginTop:'20px',marginBottom:'30px'}}>My Meeting ❯ {folder[0].folderName}</Text></Grid.Col> */}
+          <Grid.Col span={10}><Text size='30px' fw={'500'} style={{marginTop:'20px',marginBottom:'30px'}}>My Meeting ❯ {folderid}</Text></Grid.Col>
+          <Grid.Col span={1.3}><Button color='#EE5D20' variant='outline' radius={60} onClick={()=>statisticButton()} fullWidth style={{marginTop:'10px'}}>Statistic</Button></Grid.Col>
+        </Grid>
+      
+        <Grid align="center" style={{ borderBottom: '1px solid black',paddingBottom:'10px'}}>
+        <Grid.Col span={4}><Text c="#4f5b5f" style={{marginLeft:'10px'}}>Meeting Name</Text></Grid.Col>
+        <Grid.Col span={2} ><Text c="#4f5b5f">Status</Text></Grid.Col>
+        <Grid.Col span={2} ><Text c="#4f5b5f">Owner</Text></Grid.Col>
+        <Grid.Col span={2} ><Text c="#4f5b5f">Meeting Date</Text></Grid.Col>
+        </Grid>
+
+        <div>
+            {meeting.map((meeting) => (
+            <MeetingCard meeting = {meeting} user={session.user} key={meeting.MeetId}/>
+          ))}
+            </div>
+        </div>
+           <div style={{height:'10px', backgroundColor:'#EE5D20',position: 'fixed',bottom: '0', width: '100%'}}></div> 
+        </>:<></>
+     } 
+    </div></div>
+    
+  )
+}
+
+export default SharedMeetingList
