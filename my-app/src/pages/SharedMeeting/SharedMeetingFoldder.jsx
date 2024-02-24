82f8d5b1fa779590eed22980886ba7eae9aa5aab
tnanun <tananun_c@cmu.ac.th>
2024-02-24 15:59:51 +0700

    rewrite delete meet, remove unnecessary line, link

--- my-app/src/pages/SharedMeeting/SharedMeetingFoldder.jsx
+++ my-app/src/pages/SharedMeeting/SharedMeetingFoldder.jsx
@@ -1,40 +1,61 @@
-// import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import React from 'react'
+import { useParams, Link } from 'react-router-dom'
+import { Grid, GridCol, SegmentedControl, Text, rem, Button } from '@mantine/core';
 import { supabase } from '../../supabaseClient.js';
-import Navbar from '../../components/Navbar/Navbar.jsx'
+import { useEffect, useState } from "react";
+import MeetingCard from 'src/components/MeetingCard';
+import { useSession } from '@supabase/auth-helpers-react';
 
-function SharedMeeting() {
-  // const navigate = useNavigate();
+function SharedMeetingFoldder() {
+const { id } = useParams();
+const session = useSession();
+const [user, setUser] = useState({});
+const [newMeeting, setNewMeeting] = useState([]);
 
-    const [user, setUser] = useState({});
+useEffect(() =>{
+    async function getUserData() {
+      await supabase.auth.getUser().then((value) =>{
+        // value.data.user
+        if(value.data?.user){
+          console.log(value.data.user)
+          setUser(value.data.user)
+        }
+      })
+    }
+    getUserData();
+    getNewMeeting();
+  }, [])
 
-    useEffect(() =>{
-      async function getUserData() {
-        await supabase.auth.getUser().then((value) =>{
-          // value.data.user
-          if(value.data?.user){
-            console.log(value.data.user)
-            setUser(value.data.user)
-          }
-        })
+  async function getNewMeeting() {
+    try {
+      const { data, error } = await supabase
+        .from("meeting")
+        .select("*")
+        .eq("folderId", id)
+        //จริงๆๆต้องเอาแค่ meeting ที่พึ่งสร้างใหม่
+      if (error) throw error;
+      if (data != null) {
+        console.log("get New Meeting", data);
+        setNewMeeting(data); 
       }
-      getUserData();
-    }, [])
-      
-    return (
-      <div className="App">
-        {Object.keys(user).length !== 0 ?
-          <>
-        <header>
-        <Navbar props={user}/>
-        </header>
-       <h1>This is SharedMeeting Page</h1>
-       </>
-       :
-       <></>
-       }
-      </div>
-    );
+    } catch (error) {
+      alert(error.message);
+    }
   }
-  
-  export default SharedMeeting;
+
+  return (
+    
+    <div>
+        <Link to="/SharedMeeting" ><Button color='#EE5D20' radius="xl">Back</Button></Link>
+        <br />
+        Folder
+        <div className=''>
+            {newMeeting.map((newMeeting) => (
+            <MeetingCard meeting = {newMeeting} user = {user} />
+          ))}
+            </div>
+    </div>
+  )
+}
+
+export default SharedMeetingFoldder;
