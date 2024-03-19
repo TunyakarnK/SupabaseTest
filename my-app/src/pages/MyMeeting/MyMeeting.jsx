7a163a953aefa22b961f56ebd9a29aff55f9cbb7
tnanun <tananun_c@cmu.ac.th>
2024-03-19 07:47:25 +0700

    add code feature fetch meet from google calendar
    MyMeeting.jsx

--- my-app/src/pages/MyMeeting/MyMeeting.jsx
+++ my-app/src/pages/MyMeeting/MyMeeting.jsx
@@ -10,6 +10,11 @@ import { Grid, ScrollArea, TextInput,Text,rem, Button,Modal ,Radio,Group} from '
 import { useDisclosure } from '@mantine/hooks';
 import { useSession } from '@supabase/auth-helpers-react';
 
+const CLIENT_ID = import.meta.env.VITE_CALENDAR_ID;
+const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
+const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
+
 function MyMeeting() {
   const navigate = useNavigate();
   const session = useSession()
@@ -22,11 +27,15 @@ function MyMeeting() {
   const [newMeetName, setNewMeetName] = useState([]);
   const [ folderOwner, setFolderOwner ] = useState(null);
 
+  const [gapiInited, setGapiInited] = useState(false);
+  const [gisInited, setGisInited] = useState(false);
+  const [tokenClient, setTokenClient] = useState(null);
+
   useEffect(() =>{
     getUserData();
     fetchFolder();
     fetchNewMeeting();
-    console.log('meet'+newMeeting)   
+    console.log('meet'+newMeeting)
   }, [session])
 
   async function getUserData() {
@@ -149,6 +158,151 @@ function MyMeeting() {
       }
     }
 
+    useEffect(() => {
+      const script1 = document.createElement('script');
+      script1.src = 'https://apis.google.com/js/api.js';
+      script1.async = true;
+      script1.defer = true;
+      script1.onload = gapiLoaded;
+      document.body.appendChild(script1);
+  
+      const script2 = document.createElement('script');
+      script2.src = 'https://accounts.google.com/gsi/client';
+      script2.async = true;
+      script2.defer = true;
+      script2.onload = gisLoaded;
+      document.body.appendChild(script2);
+  
+      return () => {
+        document.body.removeChild(script1);
+        document.body.removeChild(script2);
+      };
+    }, []);
+  
+    const gapiLoaded = () => {
+      window.gapi.load('client', initializeGapiClient);
+    };
+  
+    const initializeGapiClient = async () => {
+      await gapi.client.init({
+        apiKey: API_KEY,
+        discoveryDocs: [DISCOVERY_DOC],
+      });
+      setGapiInited(true);
+    };
+  
+    const gisLoaded = () => {
+      const tempTokenClient = google.accounts.oauth2.initTokenClient({
+        client_id: CLIENT_ID,
+        scope: SCOPES,
+        callback: '', // defined later
+      });
+      console.log(tempTokenClient);
+      setTokenClient(tempTokenClient);
+      setGisInited(true);
+    };
+  
+    const handleAuthClick = () => {
+      if (!tokenClient) return;
+  
+      tokenClient.callback = async (resp) => {
+        if (resp.error !== undefined) {
+          throw resp;
+        }
+        // document.getElementById('signout_button').style.visibility = 'visible';
+        document.getElementById('authorize_button').innerText = 'Refresh';
+        await listUpcomingEvents();
+      };
+  
+      if (gapi.client.getToken() === null) {
+        tokenClient.requestAccessToken({ prompt: 'consent' });
+      } else {
+        tokenClient.requestAccessToken({ prompt: '' });
+      }
+    };
+  
+    // const handleSignoutClick = () => {
+    //   const token = gapi.client.getToken();
+    //   if (token !== null) {
+    //     google.accounts.oauth2.revoke(token.access_token);
+    //     gapi.client.setToken('');
+    //     document.getElementById('content').innerText = '';
+    //     document.getElementById('authorize_button').innerText = 'Authorize';
+    //     // document.getElementById('signout_button').style.visibility = 'hidden';
+    //   }
+    // };
+  
+    const listUpcomingEvents = async () => {
+      let response;
+      try {
+        const request = {
+          calendarId: 'primary',
+          timeMin: new Date().toISOString(),
+          showDeleted: false,
+          singleEvents: true,
+          maxResults: 10,
+          orderBy: 'startTime',
+        };
+        response = await gapi.client.calendar.events.list(request);
+        console.log("ข้อมูลที่ดึงมา", response);
+        console.log("Meeting_name", response.result.items[1].summary);
+        console.log("meetStartDate", response.result.items[1].start.dateTime);
+        const datetimeString = response.result.items[1].end.dateTime;
+        const dateTime = new Date(datetimeString);
+
+        // Extracting time portion
+        const timeString = dateTime.toLocaleTimeString('en-US', {
+          hour12: false, // Use 24-hour format
+          hour: '2-digit',
+          minute: '2-digit',
+          second: '2-digit'
+        });
+
+        // console.log(timeString);
+        console.log("meetEndDate", timeString);
+        console.log("meetDes", response.result.items[1].description);
+        console.log("meetCreate", response.result.items[1].created);
+        console.log("meetCreate", response.result.items[1].creator.email);
+        console.log("cal_id", response.result.items[1].id);
+        // insert meeting from calendar to meeting table and add attendee
+        if (response.result.items.length != 0){
+          for(var i = 0; i <= response.result.items.length; i++){
+            supabase
+            .from("meeting")
+            .insert({
+              meetName: response.result.items[i].summary,
+              meetStartDate: response.result.items[i].start.dateTime,
+              meetEndDate: timeString,
+              meetDes: response.result.items[i].description,
+              meetCreate: response.result.items[i].created,
+              creatorId: session.user.id,
+              cal_id: response.result.items[i].id
+            })
+            .select()
+            .single()
+              .then((result) => {
+                console.log("fetch new meeting from google calendar", result);
+                supabase
+                .from("attendee")
+                .insert({
+                  meetId: result.data.meetId,
+                  email: session.user.email,
+                  userId: session.user.id
+                })
+                .then((result) => {
+                  console.log("insert Attendee", result);
+                  window.location.reload(); 
+                })
+              fetchNewMeeting();
+            })
+          }
+      }
+      } catch (err) {
+        document.getElementById('content').innerText = err.message;
+        return;
+      }
+    };
+
     
 
     return (
@@ -161,8 +315,10 @@ function MyMeeting() {
 
        <div style={{backgroundColor:'#FDEFE9', margin:"50px", padding:'20px'}}>
         <Grid align="center">
-        <Grid.Col span={10.4}><Text size='30px' fw={'500'} style={{marginTop:'20px',marginBottom:'30px'}}>New Meeting</Text></Grid.Col>
-        <Grid.Col span={1}><Button color='#EE5D20' radius={60} onClick={()=>createNewMeeting()} style={{marginTop:'30px'}}> + New Meeting</Button></Grid.Col>
+        <Grid.Col span={10}><Text size='30px' fw={'500'} style={{marginTop:'20px',marginBottom:'30px'}}>New Meeting</Text></Grid.Col>
+        <Grid.Col span={0}><Button color='#EE5D20' radius={60} id="authorize_button" onClick={handleAuthClick} style={{ visibility: gapiInited && gisInited ? 'visible' : 'hidden' }}>Sync</Button></Grid.Col>
+        {/* <Grid.Col span={0.5}><Button id="signout_button" onClick={handleSignoutClick} style={{ visibility: 'hidden' }}>Sign Out</Button></Grid.Col> */}
+        <Grid.Col span={0.5}><Button color='#EE5D20' radius={60} onClick={()=>createNewMeeting()} style={{marginTop:'30px'}}> + New Meeting</Button></Grid.Col>
         </Grid>
         <Grid align="center" style={{ borderBottom: '1px solid black',paddingBottom:'10px'}}>
         <Grid.Col span={5}><Text c="#4f5b5f" style={{marginLeft:'10px'}}>Meeting Name</Text></Grid.Col>
